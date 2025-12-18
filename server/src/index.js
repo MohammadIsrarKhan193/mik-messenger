ef0efed09969226e4e00646801e48a08a6555d5a
MohammadIsrarKhan193 <smartboy35th@gmail.com>
2025-12-18 21:16:15 +0430

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -6,16 +6,13 @@ const { setupWSServer } = require('./wsServer');
 
 const PORT = process.env.PORT || 3000;
 
-// simple HTTP server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('MÎK Messenger WebSocket Server Running');
 });
 
-// WebSocket setup
 setupWSServer(server);
 
-// start server
 server.listen(PORT, () => {
   console.log(`✅ MÎK WS server listening on port ${PORT}`);
 });
