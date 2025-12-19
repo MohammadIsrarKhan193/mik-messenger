a6a7549575db5e5660f7a52aa950f9c8c4af67ab
MohammadIsrarKhan193 <smartboy35th@gmail.com>
2025-12-19 21:52:45 +0430

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,30 +1,52 @@
-
 let ws;
 let user;
+let room = "global";
 
 function join() {
-  user = document.getElementById("username").value;
+  user = document.getElementById("username").value.trim();
   if (!user) return alert("Enter username");
 
   ws = new WebSocket("wss://mik-messenger.onrender.com");
 
   ws.onopen = () => {
-    ws.send(JSON.stringify({ type: "join", user }));
+    ws.send(
+      JSON.stringify({
+        type: "join",
+        user: user,
+        room: room,
+      })
+    );
+
+    document.getElementById("joinBox").style.display = "none";
     document.getElementById("chat").style.display = "block";
   };
 
   ws.onmessage = (e) => {
-  console.log("RAW FROM SERVER:", e.data);
-  const msg = JSON.parse(e.data);
-  console.log("PARSED:", msg);
+    const msg = JSON.parse(e.data);
     const box = document.getElementById("messages");
-    box.innerHTML += `<div><b>${msg.user}:</b> ${msg.message}</div>`;
+
+    if (msg.type === "msg") {
+      box.innerHTML += `<div><b>${msg.user}:</b> ${msg.message}</div>`;
+    }
+
+    if (msg.type === "system") {
+      box.innerHTML += `<div style="color:gray"><i>${msg.message}</i></div>`;
+    }
+
     box.scrollTop = box.scrollHeight;
   };
 }
 
-function send() {
+function sendMsg() {
   const input = document.getElementById("msg");
-  ws.send(JSON.stringify({ type: "msg", message: input.value }));
+  if (!input.value) return;
+
+  ws.send(
+    JSON.stringify({
+      type: "msg",
+      message: input.value,
+    })
+  );
+
   input.value = "";
 }
