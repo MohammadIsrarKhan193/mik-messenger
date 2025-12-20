2d6da3dc4064bc694c6313e5c87ea44384da465a
MohammadIsrarKhan193 <smartboy35th@gmail.com>
2025-12-20 20:52:45 +0430

    Create Message.js

---
+++ server/src/models/Message.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  room: {
+    type: String,
+    default: "global",
+  },
+  user: String,
+  text: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Message", messageSchema);
