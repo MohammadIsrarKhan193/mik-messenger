72168e84746a3541c207ead5c8e6d715800053ae
MohammadIsrarKhan193 <smartboy35th@gmail.com>
2026-05-06 08:34:14 +0430

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
-const { setupWSServer } = require("./wsserver");
+const { setupWSServer } = require("./wsServer");
 
 const PORT = process.env.PORT || 10000;
 
